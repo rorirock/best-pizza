026e697cff748e5b945a99ba76c2e0f2ca37d18c
rodrigo H. jimenez <rodri-31071@hotmail.com>
2021-05-13 17:20:06 -0500

    version movil login lista

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: '', redirectTo: 'login', pathMatch: 'full' },
+   { path: 'listRestaurants', loadChildren: () => import('./modules/list-restaurants/list-restaurants.module').then(m => m.ListRestaurantsModule) },
+   { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
